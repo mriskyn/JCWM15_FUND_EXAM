b398b6362276ce094f7df51fd4b271adf2d75d83
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-11-16 11:54:28 +0700

    done

---
+++ Risky-JCWM15/database.js
@@ -0,0 +1,20 @@
+let dataUsers = [
+    {
+        id: 1,
+        username: 'alee',
+        email: 'alee@gmail.com',
+        role: 'admin'
+    },
+    {
+        id: 2,
+        username: 'jennie',
+        email: 'jenniebp@gmail.com',
+        role: 'user'
+    },
+    {
+        id: 3,
+        username: 'elsa',
+        email: 'elsa@disney.com',
+        role: 'user'
+    },
+]
